3d110a8c56580233626253647e7464843a0a6baa
Marta <martabarrachina1@hotmail.com>
2017-10-02 16:55:47 +0200

    He creat tots els botons del telefon, i ara es veu cada número clicat al Label.

--- app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/fakephone/FakePhoneActivity.java
@@ -5,20 +5,37 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class FakePhoneActivity extends AppCompatActivity {
+
+    EditText total_phone;
+
+    private int numbers [] = {
+        R.id.btn0, R.id.btn1
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fake_phone);
+
+        total_phone = (EditText) findViewById(R.id.label_phone);
+
     }
 
-    public void clicat (View v){
+
+
+    public void click (View v){
         Button boto = (Button) v; // convertim el widget (v)que es un view qualsevol a en un botó
-        Log.i("click", "Has clicat el botó");
+        String s = boto.getText().toString();
+
+        total_phone.setText(s);
+
 
 
     }
+
+
 
 }
